72987d709c17e888986e8e880d6f084589972eb8
sarah6799 <70384214+sarah6799@users.noreply.github.com>
2022-05-15 20:31:05 +0930

    Create shop.php

---
+++ Controller/shop.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link  rel="stylesheet" href="style.css">
+    <title>Darwin Art</title>
+</head>
+
+<body>
+    <!--navigation-->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light py-4">
+      <div class="container">
+        <a class="navbar-brand" href="#">Darwin Art</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link" href="index.html">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="shop.html">Shop</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Cart & Track</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">About Us</a>
+            </li>
+          </ul>
+          <form class="d-flex">
+            <a href="login.html" class="button">log in/sign up</a>
+          </form>
+        </div>
+      </div>
+    </nav>
+
+
+<!--products-->
+    <div class="product1">
+        <div class="p-header">
+          <h3>Top selling</h3>
+        </div>
+
+        <div class="p-body">
+          <h5 class="card-title">Special</h5>
+          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+          <a href="#" class="btn btn-primary">add to cart</a>
+
+          <h5 class="card-title">Special</h5>
+          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+          <a href="#" class="btn btn-primary">add to cart</a>
+
+          <h5 class="card-title">Special</h5>
+          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+          <a href="#" class="btn btn-primary">add to cart</a>
+        </div>
+
+    </div>
+
+    <div class="product2">
+        <div class="p-header">
+            <h3>recommended items</h3>
+        </div>
+
+      <section class="cards">
+        <div class="Items">
+          <img src="images/2.jpg" alt="Cinque Terre" width="150" height="150">
+          <div class="desc">Pirce: AUD$ <br> description </div>
+          <a href="cart.html" class="add" target="_blank">add to cart</a>
+        </div>
+
+        <div class="Items">
+          <img src="images/2.jpg" alt="Forest" width="150" height="150">
+          <div class="desc">Pirce: AUD$ <br> description </div>
+          <a href="cart.html" class="add" target="_blank">add to cart</a>
+        </div>
+
+        <div class="Items">
+          <img src="images/2.jpg" alt="Northern Lights" width="150" height="150">
+          <div class="desc">Pirce: AUD$ <br> description </div>
+          <a href="cart.html" class="add" target="_blank">add to cart</a>
+        </div>
+
+        <div class="Items">
+          <img src="images/2.jpg" alt="Northern Lights" width="150" height="150">
+          <div class="desc">Pirce: AUD$ <br> description </div>
+          <a href="cart.html" class="add" target="_blank">add to cart</a>
+        </div>
+
+        <div class="Items">
+          <img src="images/2.jpg" alt="Northern Lights" width="150" height="150">
+          <div class="desc">Pirce: AUD$ <br> description </div>
+          <a href="cart.html" class="add" target="_blank">add to cart</a>
+        </div>
+
+        <div class="Items">
+          <img src="images/2.jpg" alt="Northern Lights" width="150" height="150">
+          <div class="desc">Pirce: AUD$ <br> description </div>
+          <a href="cart.html" class="add" target="_blank">add to cart</a>
+        </div>
+
+        <div class="Items">
+          <img src="images/2.jpg" alt="Northern Lights" width="150" height="150">
+          <div class="desc">Pirce: AUD$ <br> description </div>
+          <a href="cart.html" class="add" target="_blank">add to cart</a>
+        </div>
+
+        <div class="Items">
+          <img src="images/2.jpg" alt="Northern Lights" width="150" height="150">
+          <div class="desc">Pirce: AUD$ <br> description </div>
+          <a href="cart.html" class="add" target="_blank">add to cart</a>
+        </div>
+      </section>
+    </div>
+     
+
+
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+
+    
+</body>
+</html>
