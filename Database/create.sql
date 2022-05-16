ebbcc58820a7a678f56de155ef40142f6550373c
sarah6799 <70384214+sarah6799@users.noreply.github.com>
2022-05-16 14:45:15 +0930

    Update create.sql

--- Database/create.sql
+++ Database/create.sql
@@ -1,8 +1,51 @@
-create database DarwinABC_db DEFAULT CHARACTER SET utf8;
+create database DarwinABC_db; 
+use  DarwinABC_db;
 
-create table categories (
-    categories_id int(100) NOT NULL,
-    categories_title text NOT NULL
-)ENGINE=InnoDB; 
+create table Customer (
+    CustEmail varchar(100) NOT NULL,
+    CustFName varchar(100) NOT NULL,
+    CustLName varchar(100) NOT NULL,
+    Title text,
+    Address varchar(100) NOT NULL,
+    City varchar(100) NOT NULL,
+    State varchar(100) NOT NULL,
+    Country varchar(100) NOT NULL,
+    Postcode varchar(100) NOT NULL,
+    Phone integer(100) NOT NULL UNIQUE,
+    CONSTRAINT PK_Customer PRIMARY KEY (CustEmail)
+)ENGINE=InnoDB, DEFAULT CHARACTER SET utf8;
+
+create table Purchase (
+    PurchaseNo integer(100) NOT NULL UNIQUE,
+    CustEmail varchar(100) NOT NULL,
+    Date DEFAULT GETDATE(),
+    CONSTRAINT PK_Purchase PRIMARY KEY (PurchaseNo),
+    CONSTRAINT FK_Purchase FOREIGN KEY (CustEmail)
+)ENGINE=InnoDB, DEFAULT CHARACTER SET utf8;
+
+create table PurchaseItem (
+    ItemNo integer(100) NOT NULL UNIQUE,
+    Quantity varchar(100),
+    PurchaseNo integer(100) NOT NULL UNIQUE,
+    ProductNo integer(100) NOT NULL UNIQUE,
+    CONSTRAINT PK_Purchase PRIMARY KEY (ItemNo),
+    CONSTRAINT FK_Purchase FOREIGN KEY (PurchaseNo),
+    CONSTRAINT FK_Purchase FOREIGN KEY (ProductNo)
+)ENGINE=InnoDB, DEFAULT CHARACTER SET utf8;
+
+create table Product (
+    ProductNo integer(100) NOT NULL UNIQUE,
+    Description text(100),
+    Price integer(100) Not NULL,
+    Category varchar(100) Not NULL,
+    Color text(100),
+    Size integer(100),
+    CONSTRAINT PK_Purchase PRIMARY KEY (ProductNo)
+)ENGINE=InnoDB, DEFAULT CHARACTER SET utf8;
+
+
+
+
+
 
 
