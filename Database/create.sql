f7ffffe56d6905ed31ba0a3287d4f9437d50d25e
sarah6799 <70384214+sarah6799@users.noreply.github.com>
2022-05-16 13:22:25 +0930

    Update create.sql

--- Database/create.sql
+++ Database/create.sql
@@ -1,7 +1,8 @@
-create database DarwinABC_db
+create database DarwinABC_db DEFAULT CHARACTER SET utf8;
 
 create table categories (
     categories_id int(100) NOT NULL,
     categories_title text NOT NULL
-)ENGINE=InnoDB, default CHARACTER SET utf8;
+)ENGINE=InnoDB; 
+
 
